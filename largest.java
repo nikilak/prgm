e50b946a6a6d0375ec50b5e8360992675a793439
nikilak <nikila.kumar18@gmail.com>
2016-07-06 16:15:11 +0530

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+public class largest
+{
+publis static void main(String args[])
+{
+int a,b,c;
+Scanner sc=new Scanner(System.in)
+a=sc.nextInt();
+b=sc.nextInt();
+c=sc.nextInt();
+if(a>b&&a>c)
+{
+System.out.println("a is greater");
+}
+else if(b>c&&b>a)
+{
+System.out.println("b is greater");
+}
+else
+{
+System.out.println("c is greater");
+}
+}
+}
