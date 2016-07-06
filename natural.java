e898e161be36047cfa800fd7a2dbd0c59694d1d7
nikilak <nikila.kumar18@gmail.com>
2016-07-06 16:40:23 +0530

    Create natural.java

---
+++ natural.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class natural
+{
+public static void main(String args[])
+{
+int sum=0,i;
+for(i=0;i<100;i++)
+{
+sum=sum+i;
+System.out.println(sum);
+}
+}
+}
